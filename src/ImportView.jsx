3d5467d5a88d766259430acefdf885c38db69f5e
kimolas <4828831+kimolas@users.noreply.github.com>
2026-03-21 04:40:36 -0400

    fix build error

--- src/ImportView.jsx
+++ src/ImportView.jsx
@@ -29,7 +29,6 @@ const ImportView = ({ onImport, onCancel }) => {
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50">
-      <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50">
       <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white dark:bg-slate-800 dark:border-slate-700">
         <div className="mt-3 text-center">
           <h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">Import Huckleberry CSV</h3>
